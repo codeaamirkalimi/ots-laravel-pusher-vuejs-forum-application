e6af42897be71d7717c5073108ec134ca6f441c0
Aamir Kalimi <aamirkalimi.edu@gmail.com>
2021-05-12 12:24:45 +0530

    model relationships added

--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Reply extends Model
 {
     use HasFactory;
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function question(){
+        return $this->belongsTo(Question::class);
+    }
+    public function likes(){
+        return $this->hasMany(Like::class);
+    }
 }
